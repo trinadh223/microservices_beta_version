58c04b7892b0aed1aeec781b4033a5ce672cfacb
rohit <amba.rohit8@gmail.com>
2019-06-03 17:07:14 +0530

    auto debit revoke access

--- src/main/java/com/example/demo/CheckBalance.java
+++ src/main/java/com/example/demo/CheckBalance.java
@@ -17,24 +17,51 @@ public class CheckBalance {
         return responseData;
     }
 
-    public void setResponseData(String responseData) {
-        this.responseData = responseData;
+    public CheckBalance() {
     }
-    TransactionStatusResponse txnstatres;
-    ValidateTokenResponse valt;
-    final String userToken = "Delvit07224170213556";
-    final String totalAmount = "REFUND";
-    final String mid = "ORDER_123456789";
-    final String txnId = txnstatres.getTXNID();
-    final String refId = valt.getMobile();
-    final String refundAmount = "1.00";
-    final String MERCHANT_KEY = "&!vj74@Ri&g6U1TI";
+    ValidateOtpResponse votp;
+     String mid = "Delvit07224170213556";
+     String orderId = "ORDER_123456789";
+     String userToken = votp.getAccess_token();
+     String totalAmount = "1.00";
+     String MERCHANT_KEY = "&!vj74@Ri&g6U1TI";
+    public void check_balance(){
+        TreeMap<String, String> paytmParams = new TreeMap<String, String>();
+        paytmParams.put("MID", mid);
+        paytmParams.put("userToken", userToken);
+        paytmParams.put("totalAmount", totalAmount);
+
+        try {
+            URL url = new URL("https://securegw-stage.paytm.in/paymentservices/pay/consult"); // for staging
+            String checksum = CheckSumServiceHelper.getCheckSumServiceHelper().genrateCheckSum(MERCHANT_KEY, paytmParams);
+            String paytmParams_head = "{\"clientId\":\"merchant-perpule-stg\",\"version\":\"V1\",\"requestTimestamp\":\""+votp.getExpires()+"\",\"channelId\":\"WAP\",\"signature\":\""+checksum+"\"}";
+            String paytmParams_body = "{\"userToken\":\"" + userToken + "\",\"totalAmount\":\""+totalAmount+"\",\"mid\":\""+mid+"\"}";
+            String post_data = "{\"body\":" + paytmParams_body + ",\"head\":" + paytmParams_head + "}";
+
+
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            DataOutputStream requestWriter = new DataOutputStream(connection.getOutputStream());
+            requestWriter.writeBytes(post_data);
+            requestWriter.close();
+            InputStream is = connection.getInputStream();
+            BufferedReader responseReader = new BufferedReader(new InputStreamReader(is));
+            if ((responseData = responseReader.readLine()) != null) {
+                System.out.append("Response: " + responseData);
+            }
+            // System.out.append("Request: " + post_data + " ");
+            responseReader.close();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
 
 
 
 
-
-
-
-
+    }
 }
